3d50942aa2128e369e1262adfcec8ffc79e6ad1e
Peterson Batatwenda <112376571+BataPeter@users.noreply.github.com>
2022-09-19 20:32:00 +0300

    Added some changes

--- functions.js
+++ functions.js
@@ -10,7 +10,7 @@
 //    console.log (ans)
 //    return num2;
 //    return ans;
-  
+
 //  }
 //  // function call aslo technically referred to as function invocation
 //  add ();
@@ -30,22 +30,52 @@
 
 // function testExample(){
 //     var greeting = 'Hi brother';
-    
+
 //     console.log(greeting);
 // }
 // testExample();
 
-function netPay(){
-    var gross =1000000; //static value
-    var tax =1000000 * 0.3
-    var actualPay =gross - tax;
-return actualPay;
+function netPay() {
+  var gross = 1000000; //static value
+  var tax = 1000000 * 0.3;
+  var actualPay = gross - tax;
+  return actualPay;
 }
 
-function welfare(){
-    var welFare =50000;
-    var takeHome =netPay() - welFare;
-    console.log(takeHome);
+function welfare() {
+  var welFare = 50000;
+  var takeHome = netPay() - welFare;
+  console.log(takeHome);
 }
 welfare();
+
+//Define 7 classes with at least 5 properties using <this> keyword and write at least 5 instances using the <new> keyword
+
+// -ratings, name, location, stars.
+class Hotel {
+  constructor(ratings, name, location, stars, owner) {
+    this.name = name;
+    this.ratings = ratings;
+    this.location = location;
+    this.owner = owner;
+    this.stars = stars;
+  }
+   checkRating(){
+    if(this.ratings >= 4){
+      console.log("excellent")
+    } else if(this.ratings >=2 && this.ratings <4){
+      console.log("average")
+    }
+    else {
+      console.log("poor")
+    }
+  }
+}
+let sheraton = new Hotel(4, "sheraton", "kampala", "5 star", "janet")
+let jeju = new Hotel(1, "jeju", "nansana", "1 star", "mategyero")
+console.log(sheraton.name)
+console.log(sheraton.owner)
+sheraton.checkRating()
+jeju.checkRating()
+
 
