17e650e2a47863e8c5ea8a90c0e61bb0b7dae4f6
Peterson Batatwenda <112376571+BataPeter@users.noreply.github.com>
2022-09-21 13:20:53 +0300

    More work added to my files

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = "fuchsia";
+const html = document.documentElement;
+console.log(html.parentNode);
+//console.log(document.getElementsByTagName("ul").childNodes);
