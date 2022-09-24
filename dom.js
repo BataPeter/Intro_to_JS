cca9af94124b8eb98b02518dd45dc063285ae40c
Peterson B Mwesigye <mwesigyepeterson38@gmail.com>
2022-09-24 08:35:50 +0300

    Some changes

--- dom.js
+++ dom.js
@@ -1,4 +1,30 @@
-document.body.style.backgroundColor = "fuchsia";
-const html = document.documentElement;
-console.log(html.parentNode);
-//console.log(document.getElementsByTagName("ul").childNodes);
+// document.body.style.backgroundColor = "fuchsia";
+// const html = document.documentElement;
+// console.log(html.parentNode);
+// //console.log(document.getElementsByTagName("ul").childNodes);
+
+const h1 = document.getElementsByTagName("h1")[0];
+const p = document.getElementsByTagName("p")[0];
+const ul = document.getElementsByTagName("ul")[0];
+console.log(p.parentNode);
+
+console.log(p.parentNode);
+console.log(p.parentNode.parentNode.parentNode.parentNode);
+console.log(ul.childNodes);
+
+ul.firstElementChild.style.background = "yellow";
+for (let Element of ul.children) {
+  Element.style.background = "gold";
+}
+document.body.children[3].lastElementChild.style.background = "fuchsia";
+const tiger = ul.children[1];
+tiger.nextElementSibling.style.background = "coral";
+tiger.previousElementSibling.style.background = "aquamarine";
+
+//Add other sharks
+for (let element of ul.children) {
+  const sharkList = document.querySelector("ul");
+}
+const newShark = document.createElement("li");
+newShark.textContent = "Serah Shark";
+sharkList.appendChild(newShark);
--- self_taught.js
+++ self_taught.js
@@ -1,113 +1,154 @@
 var myNumbr = 88;
-    myNumbr = myNumbr --
-    console.log (myNumbr)
-
+myNumbr = myNumbr--;
+console.log(myNumbr);
 
 let x = 10;
 x += 5;
-console.log (x);
+console.log(x);
 
-let p = 20, q = 40;
-let r = p/=q
-console.log (r);
-
+let p = 20,
+  q = 40;
+let r = (p /= q);
+console.log(r);
 
 let text1 = "Simon";
 let text2 = "Peter";
 let text3 = text1 + " " + text2;
-console.log (text3);
+console.log(text3);
 
 let text4 = "What a very ";
 text4 += "nice day!";
-console.log (text4);// how do I declare this so I can get an answer in the terminal?
+console.log(text4); // how do I declare this so I can get an answer in the terminal?
 
 //Adding strings and numbers
 let a = 5 + 5;
 let b = "5" + 5;
 let c = "Hello" + 5;
-console.log (a);
-console. log (b);
-console.log (c);
+console.log(a);
+console.log(b);
+console.log(c);
 
 // how do I see this in the terminal
-  
-  //if (condition) 
-  //  block of code to be executed if the condition is true
-//} else 
-    //  block of code to be executed if the condition is false
 
-    let rabbits= "16" + "Volvo"; //When adding a number and a string, JavaScript will treat the number as a string.
-    console.log (rabbits)
-    
-    let cathy = 16 + "Volvo";
-    console.log (cathy)
+//if (condition)
+//  block of code to be executed if the condition is true
+//} else
+//  block of code to be executed if the condition is false
 
-    let hannah = "Volvo" + 16;
-    console.log (hannah);
+let rabbits = "16" + "Volvo"; //When adding a number and a string, JavaScript will treat the number as a string.
+console.log(rabbits);
 
-    let liz = 16 + 4 + "Volvo";
-    console.log (liz)
+let cathy = 16 + "Volvo";
+console.log(cathy);
 
-    var difference = 45 - 10;
-    console.log (difference)
+let hannah = "Volvo" + 16;
+console.log(hannah);
 
-    var product = 8 * 10;
-    console.log (product);
-    var quotient = 66 / 33;
-    console.log (quotient)
+let liz = 16 + 4 + "Volvo";
+console.log(liz);
 
-    var myDecimal = 4.8 * 2;
-    console.log (myDecimal)
+var difference = 45 - 10;
+console.log(difference);
 
-    var remainder;
-    remainder = 11 % 3;
-    console.log (remainder)
+var product = 8 * 10;
+console.log(product);
+var quotient = 66 / 33;
+console.log(quotient);
 
-    var bread = 3;
-    var nuts = 17;
-    var peanut = 12;
-    var myString = "firstLine\n\t\\secondLine\nThirdLine"
-    console.log (myString)
+var myDecimal = 4.8 * 2;
+console.log(myDecimal);
 
-    var ourStr = "I come first." + "I come second.";
-    console.log (ourStr)
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
 
-    var myStr ="This is the first sentence. "
-    myStr += "This is the second sentence."
-    console.log (myStr)
+var bread = 3;
+var nuts = 17;
+var peanut = 12;
+var myString = "firstLine\n\t\\secondLine\nThirdLine";
+console.log(myString);
 
-    var myName ="Owakubariho";
-    var myOther ="My name is " + myName + " and I am well!"
-    console.log (myOther)
+var ourStr = "I come first." + "I come second.";
+console.log(ourStr);
 
-    var anAdjective = "Awesome!";
-    var ourStr = "FreeCodeCamp is ";
-    ourStr += anAdjective
-    console.log (ourStr)
-    //Strings are immutable. They cannot be altered once they are created.
-    var firstName = "Ada";
-    var lastLetterOffFirstName = firstName [firstName.length - 1];
-    var lastName = "LoveLace";
-    var lastLetterofLastName = lastName[ lastName.lenght -1]; 
-    console.log (lastName)
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+console.log(myStr);
 
+var myName = "Owakubariho";
+var myOther = "My name is " + myName + " and I am well!";
+console.log(myOther);
 
-   // var myString = 'https://www.youtube.com/"watch"?v=PkZNo7MFNFg'//
+var anAdjective = "Awesome!";
+var ourStr = "FreeCodeCamp is ";
+ourStr += anAdjective;
+console.log(ourStr);
+//Strings are immutable. They cannot be altered once they are created.
+var firstName = "Ada";
+var lastLetterOffFirstName = firstName[firstName.length - 1];
+var lastName = "LoveLace";
+var lastLetterofLastName = lastName[lastName.lenght - 1];
+console.log(lastName);
 
-    /*var myVar = 87;
+// var myString = 'https://www.youtube.com/"watch"?v=PkZNo7MFNFg'//
+
+/*var myVar = 87;
     myVar = myVar + 1;
     console.log (myVar);*/
 
-    //var myVar =87;
-    //myVar = myVar ++;
-    //console.log (myVar)//
+//var myVar =87;
+//myVar = myVar ++;
+//console.log (myVar)//
 
-    
+//let x = 5;
+//let y = 5;
+//let z = 6;
+//(x == y)       // Returns true
+//(x == z)       // Returns false
 
+//Coaching with Steve!
+//keyword class. then class name <Car>
+//special function inside of a class- a constructor
+class Car {
+  constructor(year, brand, color, origin) {
+    this.year = year;
+    this.brand = brand;
+    this.color = color;
+    this.origin = origin;
+  }
+  performance() {
+    console.log("Race car performance");
+  }
+}
 
+class Truck extends Car {
+  constructor(year, brand, color, origin, numberOfWheels, capacity) {
+    super(year, brand, color, origin);
+    this.numberOfWheels = numberOfWheels;
+    this.capacity = capacity;
+  }
+}
+let car2 = new Truck(2006, "Toyota", "Grey", "Japan", 4, 10);
+console.log(car2);
+let car1 = new Car(2022, "Benz", "Silver", "Germany");
+console.log(car2);
 
-    //let x = 5;
-    //let y = 5;
-    //let z = 6;
-    //(x == y)       // Returns true
-    //(x == z)       // Returns false
+//While Loops
+var age = 5;
+while (age < 10) {
+  console.log("your age is less than 10");
+  age++;
+}
+document.write("you are now over 10");
+
+//for loop
+for (age = 5; age < 10; age++) {
+  console.log("your age is less than 10");
+}
+document.write("you are now over 10");
+
+var links = document.getElementsByTagName("a");
+for (i = 1; i <= links.length; i++) {
+  console.log("this is link number" + i);
+}
+document.write("all links are now looped");
